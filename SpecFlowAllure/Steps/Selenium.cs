d304fbb6461d644545e499551b9b3133d5cf7ef3
TASpectrum <taspectrum@mail.ru>
2022-02-22 15:46:02 +0500

    upd6

--- SpecFlowAllure/Steps/Selenium.cs
+++ SpecFlowAllure/Steps/Selenium.cs
@@ -26,38 +26,59 @@ namespace SpecFlowAllure
         [When(@"откроем google")]
         public void WhenОткроемGoogle()
         {
-            var browserDriverPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            ChromeOptions chromeOptions = new ChromeOptions();
-            chromeOptions.AddArguments(browserDriverPath, "--no-sandbox", "--headless", "--disable-gpu");
-            //            driver = (chromeOptions.);
-            driver = new ChromeDriver(chromeOptions);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-            // driver.Manage().Window.Maximize();
-            driver.Manage().Window.Size = new Size(1920, 1080);
-            driver.Navigate().GoToUrl(@"https:\\google.ru");
-            driver.FindElement(By.Name("q")).SendKeys("mail.ru");
-            driver.FindElement(By.Name("btnK")).Click();
-
+            try
+            {
+                var browserDriverPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.AddArguments(browserDriverPath, "--no-sandbox", "--headless", "--disable-gpu");
+                //            driver = (chromeOptions.);
+                driver = new ChromeDriver(chromeOptions);
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+                // driver.Manage().Window.Maximize();
+                driver.Manage().Window.Size = new Size(1920, 1080);
+                driver.Navigate().GoToUrl(@"https:\\google.ru");
+                driver.FindElement(By.Name("q")).SendKeys("mail.ru");
+                driver.FindElement(By.Name("btnK")).Click();
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine(ex.Message);
+                driver.Quit();
+            }
 
         }
 
         [Then(@"перейдём на mailru")]
         public void ThenПерейдёмНаMailru()
         {
-            Thread.Sleep(2000);
-            driver.FindElement(By.XPath("//a[@href='https://mail.ru/']")).Click();
+            try
+            {
+                driver.FindElement(By.XPath("//a[@href='https://mail.ru/']")).Click();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                driver.Quit();
+            }
         }
 
         [Then(@"Пощёлкаем вкладки")]
         public void ThenПощёлкаемВкладки()
         {
+            try 
+            { 
             driver.SwitchTo().Window(driver.WindowHandles[1]);
-            driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-covid']")).Click();
             driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-regional']")).Click();
             driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-sport']")).Click();
             driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-lady']")).Click();
             driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-auto']")).Click();
             driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-cinema']")).Click();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                driver.Quit();
+            }
         }
 
         [Then(@"Закроем браузер")]
