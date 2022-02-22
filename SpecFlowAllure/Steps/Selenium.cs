b99c3dc7eb1dd2b7887e8c408d64f8dda979da68
TASpectrum <taspectrum@mail.ru>
2022-02-22 10:39:05 +0500

    upd1

--- SpecFlowAllure/Steps/Selenium.cs
+++ SpecFlowAllure/Steps/Selenium.cs
@@ -1,6 +1,8 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Drawing;
+using System.Threading;
 using TechTalk.SpecFlow;
 
 namespace SpecFlowAllure
@@ -8,12 +10,20 @@ namespace SpecFlowAllure
     [Binding]
     public class Selenium
     {
+
+
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+       
         WebDriver driver = new ChromeDriver();
-        [When(@"откроем google")]
+    [When(@"откроем google")]
         public void WhenОткроемGoogle()
         {
+            chromeOptions.AddArguments("no-sandbox", "headless", "disable-gpu");
+            var driver = new ChromeDriver(chromeOptions);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-            driver.Manage().Window.Maximize();
+            // driver.Manage().Window.Maximize();
+            driver.Manage().Window.Size = new Size(1920, 1080);
             driver.Navigate().GoToUrl(@"https:\\google.ru");
             driver.FindElement(By.Name("q")).SendKeys("mail.ru");
             driver.FindElement(By.Name("btnK")).Click();
@@ -23,6 +33,7 @@ namespace SpecFlowAllure
         [Then(@"перейдём на mailru")]
         public void ThenПерейдёмНаMailru()
         {
+            Thread.Sleep(2000);
             driver.FindElement(By.XPath("//a[@href='https://mail.ru/']")).Click();
         }
 
