2af41bebadd6454104f6c7377bfd92eedb070d54
TASpectrum <taspectrum@mail.ru>
2022-02-22 15:21:42 +0500

    upd

--- SpecFlowAllure/Features/Objects.feature
+++ SpecFlowAllure/Features/Objects.feature
@@ -10,98 +10,98 @@
 	Затем Пощёлкаем вкладки
 	И Закроем браузер
 
-@layer:web
-@allure.label.story:Объекты
-@allure.label.microservice:WebObjects
-@allure.label.layer:selenium
-Сценарий: Вход в объекты
-	Когда условие false
-	Тогда объект работает
-	
-@layer:web
-@story:Объекты
-@allure.label.microservice:WebObjects
-@allure.label.layer:selenium
-Сценарий: Добавление объекта
-	Когда условие false
-	Тогда объект работает
-
-@layer:web
-@story:Объекты
-@allure.label.microservice:WebObjects
-@allure.label.layer:selenium
-Сценарий: Изменение объекта
-	Когда условие true
-	Тогда объект работает
-
-@layer:web
-@story:Объекты
-@allure.label.microservice:WebObjects
-Сценарий: Удаление объекта
-	Когда условие true
-	Тогда объект работает
-
-@layer:web
-@story:Рассчеты
-@allure.label.microservice:WebCalculations
-Сценарий: Проверка рассчета
-	Когда условие false
-	Тогда объект работает
-
-@layer:web
-@story:Рассчеты
-@allure.label.microservice:WebObjects
-Сценарий: Добавление расчета
-	Когда условие true
-	Тогда объект работает
-
-@layer:web
-@story:Рассчеты
-@allure.label.microservice:WebCalculations
-Сценарий: Удаление рассчета
-	Когда условие true
-	Тогда объект работает
-
-@layer:web
-@story:Справочники
-@allure.label.microservice:WebCatalog
-@allure.label.layer:selenium
-Сценарий: Проверка справочника
-	Когда условие true
-	Тогда объект работает
-
-@layer:web
-@story:Справочники
-@allure.label.microservice:WebObjects
-@allure.label.layer:selenium
-Сценарий: Создание справочника
-	Когда условие true
-	Тогда объект работает
-
-@layer:web
-@story:Справочники
-@allure.label.microservice:WebCatalog
-@allure.label.layer:selenium
-Сценарий: Удаление справочника
-	Когда условие true
-	Тогда объект работает
-
-@layer:web
-@story:Профиль-пользователя
-@allure.label.microservice:WebCatalog
-@allure.label.layer:selenium
-Сценарий: Просмотр профиля
-	Когда условие true
-	Тогда объект работает
-
-@layer:web
-@story:Профиль_пользователя
-@allure.label.microservice:WebCatalog
-@allure.label.layer:selenium
-Сценарий: Изменение полей профиля
-	Когда условие true
-	Тогда объект работает
-
+#@layer:web
+#@allure.label.story:Объекты
+#@allure.label.microservice:WebObjects
+#@allure.label.layer:selenium
+#Сценарий: Вход в объекты
+#	Когда условие false
+#	Тогда объект работает
+#	
+#@layer:web
+#@story:Объекты
+#@allure.label.microservice:WebObjects
+#@allure.label.layer:selenium
+#Сценарий: Добавление объекта
+#	Когда условие false
+#	Тогда объект работает
+#
+#@layer:web
+#@story:Объекты
+#@allure.label.microservice:WebObjects
+#@allure.label.layer:selenium
+#Сценарий: Изменение объекта
+#	Когда условие true
+#	Тогда объект работает
+#
+#@layer:web
+#@story:Объекты
+#@allure.label.microservice:WebObjects
+#Сценарий: Удаление объекта
+#	Когда условие true
+#	Тогда объект работает
+#
+#@layer:web
+#@story:Рассчеты
+#@allure.label.microservice:WebCalculations
+#Сценарий: Проверка рассчета
+#	Когда условие false
+#	Тогда объект работает
+#
+#@layer:web
+#@story:Рассчеты
+#@allure.label.microservice:WebObjects
+#Сценарий: Добавление расчета
+#	Когда условие true
+#	Тогда объект работает
+#
+#@layer:web
+#@story:Рассчеты
+#@allure.label.microservice:WebCalculations
+#Сценарий: Удаление рассчета
+#	Когда условие true
+#	Тогда объект работает
+#
+#@layer:web
+#@story:Справочники
+#@allure.label.microservice:WebCatalog
+#@allure.label.layer:selenium
+#Сценарий: Проверка справочника
+#	Когда условие true
+#	Тогда объект работает
+#
+#@layer:web
+#@story:Справочники
+#@allure.label.microservice:WebObjects
+#@allure.label.layer:selenium
+#Сценарий: Создание справочника
+#	Когда условие true
+#	Тогда объект работает
+#
+#@layer:web
+#@story:Справочники
+#@allure.label.microservice:WebCatalog
+#@allure.label.layer:selenium
+#Сценарий: Удаление справочника
+#	Когда условие true
+#	Тогда объект работает
+#
+#@layer:web
+#@story:Профиль-пользователя
+#@allure.label.microservice:WebCatalog
+#@allure.label.layer:selenium
+#Сценарий: Просмотр профиля
+#	Когда условие true
+#	Тогда объект работает
+#
+#@layer:web
+#@story:Профиль_пользователя
+#@allure.label.microservice:WebCatalog
+#@allure.label.layer:selenium
+#Сценарий: Изменение полей профиля
+#	Когда условие true
+#	Тогда объект работает
+#
 #@layer:web
 #@story:"Поиск"
 #@allure.label.microservice:WebCatalog
--- SpecFlowAllure/Steps/Selenium.cs
+++ SpecFlowAllure/Steps/Selenium.cs
@@ -2,6 +2,8 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Drawing;
+using System.IO;
+using System.Reflection;
 using System.Threading;
 using TechTalk.SpecFlow;
 
@@ -10,23 +12,63 @@ namespace SpecFlowAllure
     [Binding]
     public class Selenium
     {
-
-
-
-        ChromeOptions chromeOptions = new ChromeOptions();
        
-        WebDriver driver = new ChromeDriver();
-    [When(@"откроем google")]
+        
+        //WebDriver driver = new ChromeDriver();
+        //ChromeOptions chromeOptions = new ChromeOptions();
+        
+        // ChromeDriverService Sdriverpath = "/usr/local/share/chromedriver";
+        //WebDriver driver = new ChromeDriver();
+        
+
+        [When(@"откроем google")]
         public void WhenОткроемGoogle()
         {
-            chromeOptions.AddArguments("no-sandbox", "headless", "disable-gpu");
-            var driver = new ChromeDriver(chromeOptions);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-            // driver.Manage().Window.Maximize();
-            driver.Manage().Window.Size = new Size(1920, 1080);
-            driver.Navigate().GoToUrl(@"https:\\google.ru");
-            driver.FindElement(By.Name("q")).SendKeys("mail.ru");
-            driver.FindElement(By.Name("btnK")).Click();
+
+            var browserDriverPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            //Set Chrome to start with maximized window
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--no-sandbox", "--headless", "--disable-gpu");
+
+            //Open Chrome
+            using (WebDriver driver = new ChromeDriver(browserDriverPath, options))
+            {
+                //Assign google.com to variable named url
+                var url = "http://google.com";
+                //Go to Google.com
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+                // driver.Manage().Window.Maximize();
+                driver.Manage().Window.Size = new Size(1920, 1080);
+                driver.Navigate().GoToUrl(@"https:\\google.ru");
+                driver.FindElement(By.Name("q")).SendKeys("mail.ru");
+                driver.FindElement(By.Name("btnK")).Click();
+                driver.FindElement(By.XPath("//a[@href='https://mail.ru/']")).Click();
+                driver.SwitchTo().Window(driver.WindowHandles[1]);
+                driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-covid']")).Click();
+                driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-regional']")).Click();
+                driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-sport']")).Click();
+                driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-lady']")).Click();
+                driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-auto']")).Click();
+                driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-cinema']")).Click();
+
+                driver.Quit();
+            }
+            
+            //   chromeOptions.AddArguments("--no-sandbox", "--headless", "--disable-gpu");
+            //            driver = (chromeOptions.);
+
+            //      driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            //      // driver.Manage().Window.Maximize();
+            //      driver.Manage().Window.Size = new Size(1920, 1080);
+            //      driver.Navigate().GoToUrl(@"https:\\google.ru");
+            //      driver.FindElement(By.Name("q")).SendKeys("mail.ru");
+            //      driver.FindElement(By.Name("btnK")).Click();
+
+
+
+
+
 
         }
 
@@ -34,25 +76,25 @@ namespace SpecFlowAllure
         public void ThenПерейдёмНаMailru()
         {
             Thread.Sleep(2000);
-            driver.FindElement(By.XPath("//a[@href='https://mail.ru/']")).Click();
+         //   driver.FindElement(By.XPath("//a[@href='https://mail.ru/']")).Click();
         }
 
         [Then(@"Пощёлкаем вкладки")]
         public void ThenПощёлкаемВкладки()
         {
-            driver.SwitchTo().Window(driver.WindowHandles[1]);
-            driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-covid']")).Click();
-            driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-regional']")).Click();
-            driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-sport']")).Click();
-            driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-lady']")).Click();
-            driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-auto']")).Click();
-            driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-cinema']")).Click();
+          // driver.SwitchTo().Window(driver.WindowHandles[1]);
+          // driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-covid']")).Click();
+          // driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-regional']")).Click();
+          // driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-sport']")).Click();
+          // driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-lady']")).Click();
+          // driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-auto']")).Click();
+          // driver.FindElement(By.XPath("//a[@data-testid='news-tabs-tab-item-cinema']")).Click();
         }
 
         [Then(@"Закроем браузер")]
         public void ThenЗакроемБраузер()
         {
-            driver.Quit();
+            //driver.Quit();
         }
     }
 }
